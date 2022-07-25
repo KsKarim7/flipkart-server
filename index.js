cae577c3aab0eee07358b60f4192075eb5de4c95
Khaled Karim <kskarim177@gmail.com>
2022-07-25 22:27:05 +0600

    users info added to db

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // const express = require('express');
 import express from 'express';
 import Connection from './database/db.js';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import defaultData from './default.js';
+import Router from './routes/route.js';
 
 const app = express();
 dotenv.config();
+
+app.use(cors())
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/', Router)
 
 const PORT = 5000;
 
